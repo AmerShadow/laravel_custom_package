e1e462ac9b429b77bdc09e5684edc4769337ba5e
Amershadow <amersohailroot@gmail.com>
2022-06-02 13:44:28 +0530

    Laravel custom packages

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Amershadow\Greetings\Http\Controllers\Greeting;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $x=new Greeting();
+    return $x->grettingMessage();
+    return view('welcome');
+});
